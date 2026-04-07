6e3dd3aeef4c192fe5998df4a0546a0c7c288a20
Krzysztof Worwa <worwak1998@gmail.com>
2026-04-07 13:29:58 +0200

    fix(seo): improve link descriptions using sr-only text

--- js/render.js
+++ js/render.js
@@ -29,6 +29,7 @@ export function createNewCard(item) {
             </div>
             <a class="card__link-wrapper" href="${item.url}" aria-label="Read more about ${item.title}">
                 <span class="card__link">Read more</span>
+                <span class="sr-only">Read more about ${item.title}</span>
                 <svg class="card__arrow" viewBox="0 0 17 14" fill="none">
                     <path d="M5.65674 12.4039L11.3136 6.74708L5.65674 1.09022" stroke="currentColor" stroke-width="1.6" stroke-linecap="round"/>
                 </svg>
